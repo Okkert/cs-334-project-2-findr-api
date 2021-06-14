eaf2e962c090e23b8266a9d0ec6da4cdbba709ac
Lauren Abrahall <49614695+laurenabrahall@users.noreply.github.com>
2021-06-14 19:07:29 +0200

    Update user.py

--- app/user.py
+++ app/user.py
@@ -145,12 +145,10 @@ def update_user_details(user_info):
 
 def delete_user(user_id):
     """Removes a user from the database
-
     Parameters
     ----------
     user_id : int
         Unique user identifier
-
     Returns
     -------
     dict
@@ -160,11 +158,20 @@ def delete_user(user_id):
         user = models.search_user(user_id)
         if user is None:
             return gen_missing("user")
-
-        status = models.delete_user_data(user_id)
-        if status is False:
-            return RESP_SERVER
-
+        # Remove dependencies
+        # Comments
+        models.remove_user_comments(user_id)
+        # Posts
+        # TODO: Remove comments on user's posts
+        models.remove_user_posts(user_id)
+        # Friends
+        models.remove_user_relationships(user_id)
+        # Members
+        models.remove_user_memberships(user_id)
+        # Notes
+        models.remove_user_notification(user_id)
+        user.delete()
+        models.commit_changes()
         return resp.RESP_OK
     except:
         return RESP_SERVER
